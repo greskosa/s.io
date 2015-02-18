78f3d9e59e734561fb88910f24602aa304ac884a
greskosa <greskos@gmail.com>
2015-02-18 15:45:48 +0200

    butoons

--- js/client/parts/StartScene.js
+++ js/client/parts/StartScene.js
@@ -15,8 +15,8 @@ define([
           this.addErrorMessage(err,position)
      }
     this.addStartBtns=function(){
-        this.addButton('./imgs/button.png','./imgs/buttonDown.png',null,{x:this.size.width/2,y:this.size.height*0.62})
-        this.addButton('./imgs/button.png','./imgs/buttonDown.png',null,{x:this.size.width/2,y:this.size.height*0.80})
+        this.addButton('./imgs/creategame.png','./imgs/creategameDown.png',null,{x:this.size.width/2,y:this.size.height*0.62})
+        this.addButton('./imgs/joingame.png','./imgs/joingameDown.png',null,{x:this.size.width/2,y:this.size.height*0.80})
      }
     this.addErrorMessage=function(msg){
       this.addText(msg,{font:"28px Verdana", fill:"#FF0000",stroke: "black", strokeThickness: 4},{x:this.size.width/2,y:this.size.height*2/3})
