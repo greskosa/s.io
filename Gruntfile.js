c267a3f33c3f952f1ff217dd26e8e0708241eb61
greskosa <greskos@gmail.com>
2015-03-27 13:05:59 +0200

    grunt

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    requirejs: {
+      compile: {
+        options:{
+            baseUrl: './js/client',
+            mainConfigFile:'js/client/main.js',
+            out:'js/client/<%= pkg.name %>.min.js',
+            include: ['main','text'],
+            preserveLicenseComments: false,
+            "optimize": "uglify2"
+        }
+      }
+    },
+
+  coffee: {
+      coffee_to_js:{
+          options:{
+            bare: true
+          },
+          expand: true,
+          flatten: false,
+          src: ["**/*.coffee"],
+          ext: ".js"
+
+}
+  }
+
+  });
+
+  // Load the plugin that provides the "uglify" task.
+//  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+
+
+  grunt.registerTask('build', ['coffee','requirejs'])
+
+
+
+
+
+
+
+  // Default task(s).
+
+// EXAMPLE
+//  grunt.registerTask("default",function(){
+//      grunt.log.writeln("Hello, "+grunt.config.get('person').name)
+//  })
+}
